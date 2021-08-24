becab4b4e50632daefa8858c5eec7fef65c730db
SUDHARSAN44 <88586834+SUDHARSAN44@users.noreply.github.com>
2021-08-24 22:23:30 +0530

    Add files via upload

---
+++ final.js
@@ -0,0 +1,48 @@
+
+let data=[]
+const abc=async(link)=>
+{
+    let api=await fetch(link)
+    let res = await api.json()
+  
+    console.log(res)
+    return res;
+}
+
+abc("http://universities.hipolabs.com/search?country=India")
+
+.then((e) => {
+    data=e.slice(0,50)
+    console.log(data)
+    myf();
+
+})
+
+.catch((err)=>{
+    console.log(err.message)
+   
+})
+function myf(){
+document.querySelector("#search"),addEventListener("input",(event=>{
+    let content1 =document.querySelector("#accordion")
+    finalData=data.filter((university)=>university.name.toLowerCase().startsWith(event.target.value.toLowerCase()))
+    console.log(finalData)
+
+    content1.innerHTML =''
+finalData.map((university) => content1.innerHTML += `
+
+  <div class="col-lg-2 col-md-2 col-sm-1">
+<div class="card text-center" style="width: 80%;margin-top: 10px; ">
+<h3> University Name :${university.name}</h3>
+  <ul class="list-group list-group-flush">
+   
+    <li class="list-group-item">Domain : ${university.domains}</li>
+    <li class="list-group-item">Country : ${university.country}</li>
+    <li class="list-group-item">Website : ${university.web_pages}</li>
+  </ul>
+</div>
+</div>
+`);
+}  
+));
+}
